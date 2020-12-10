329b7077af6216f6b5915d70e17f9839e26c2862
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-12-10 09:11:42 +0100

    Rewrite deploy tests to reduce the number of push operations

--- tests/test_deploy_bake.py
+++ tests/test_deploy_bake.py
@@ -2,6 +2,7 @@ import github
 import gitlab
 import os
 import pytest
+import pytools
 import requests
 import subprocess
 import time
@@ -21,86 +22,85 @@ def inside_bake(bake):
         os.chdir(old_path)
 
 
-@pytest.mark.deploy
-def test_github_actions_ci_on_deployed_bake(cookies):
-    bake = cookies.bake(extra_context={'github_actions_ci': 'Yes'})
+@pytools.memoize
+def remote_deploy_sha(c):
+    bake = c.bake(extra_context={'github_actions_ci': 'Yes'})
     with inside_bake(bake):
         # Push to Github
         subprocess.check_call("git remote add origin git@github.com:dokempf/test-github-actions-cookiecutter-cpp-project.git".split())
         subprocess.check_call("git push -f origin main".split())
-        bake_sha1 = subprocess.run("git rev-parse HEAD".split(), capture_output=True).stdout.decode().strip()
-
-        # Authenticate with the Github API
-        gh = github.Github(os.getenv("GH_API_ACCESS_TOKEN"))
-
-        # Find the workflow of the triggered Workflow - after giving it 2 seconds to properly initiate
+        # Push to Gitlab
+        subprocess.check_call("git remote add gitlab git@github.com:dokempf/test-github-actions-cookiecutter-cpp-project.git".split())
+        subprocess.check_call("git push -f gitlab main".split())
+        # Wait for any integrations to trigger
         time.sleep(2)
-        repo = gh.get_repo('dokempf/test-github-actions-cookiecutter-cpp-project')
+        return subprocess.run("git rev-parse HEAD".split(), capture_output=True).stdout.decode().strip()
+
+
+@pytest.mark.deploy
+def test_github_actions_ci_on_deployed_bake(cookies):
+    # Make sure that we have pushed a revision to our test repository
+    bake_sha1 = remote_deploy_sha(cookies)
+
+    # Authenticate with the Github API
+    gh = github.Github(os.getenv("GH_API_ACCESS_TOKEN"))
+
+    # Identify the correct workflow
+    repo = gh.get_repo('dokempf/test-github-actions-cookiecutter-cpp-project')
+    workflow = repo.get_workflow("ci.yml").get_runs()[0]
+    assert workflow.head_sha == bake_sha1
+
+    # Poll the workflow status
+    while workflow.status != 'completed':
+        # We poll at a relatively large interval to avoid running against the Github API
+        # limitations in times of heavy development activities on the cookiecutter.
+        time.sleep(30)
         workflow = repo.get_workflow("ci.yml").get_runs()[0]
-        assert workflow.head_sha == bake_sha1
 
-        # Poll the workflow status
-        while workflow.status != 'completed':
-            # We poll at a relatively large interval to avoid running against the Github API
-            # limitations in times of heavy development activities on the cookiecutter.
-            time.sleep(30)
-            workflow = repo.get_workflow("ci.yml").get_runs()[0]
-
-        assert workflow.conclusion == 'success'
+    assert workflow.conclusion == 'success'
 
 
 @pytest.mark.deploy
 def test_gitlab_ci_on_deployed_bake(cookies):
-    bake = cookies.bake(extra_context={'gitlab_ci': 'Yes'})
-    with inside_bake(bake):
-        # Push to Gitlab.com
-        subprocess.check_call("git remote add origin git@gitlab.com:dokempf/test-gitlab-ci-cookiecutter-cpp-project.git".split())
-        subprocess.check_call("git push -f origin main".split())
-        bake_sha1 = subprocess.run("git rev-parse HEAD".split(), capture_output=True).stdout.decode().strip()
+    # Make sure that we have pushed a revision to our test repository
+    bake_sha1 = remote_deploy_sha(cookies)
 
-        # Authenticate with Gitlab API
-        gl = gitlab.Gitlab('https://gitlab.com', private_token=os.getenv("GL_API_ACCESS_TOKEN"))
-        gl.auth()
+    # Authenticate with Gitlab API
+    gl = gitlab.Gitlab('https://gitlab.com', private_token=os.getenv("GL_API_ACCESS_TOKEN"))
+    gl.auth()
 
-        # Find the correct Gitlab pipeline - after giving it 2 seconds to properly initiate
-        time.sleep(2)
-        pipeline = gl.projects.get('dokempf/test-gitlab-ci-cookiecutter-cpp-project').pipelines.list()[0]
-        assert pipeline.sha == bake_sha1
+    # Find the correct Gitlab pipeline - after giving it 2 seconds to properly initiate
+    pipeline = gl.projects.get('dokempf/test-gitlab-ci-cookiecutter-cpp-project').pipelines.list()[0]
+    assert pipeline.sha == bake_sha1
 
-        # Poll the pipeline status
-        while pipeline.status != 'success':
-            time.sleep(5)
-            pipeline.refresh()
-            if pipeline.status in ["failed", "cancelled", "skipped"]:
-                pytest.fail("The Gitlab API reported Status '{}' while we were waiting for 'success'".format(status))
+    # Poll the pipeline status
+    while pipeline.status != 'success':
+        time.sleep(5)
+        pipeline.refresh()
+        if pipeline.status in ["failed", "cancelled", "skipped"]:
+            pytest.fail("The Gitlab API reported Status '{}' while we were waiting for 'success'".format(status))
 
 
 @pytest.mark.deploy
 def test_readthedocs_deploy(cookies):
-    bake = cookies.bake(extra_context={'readthedocs': 'Yes'})
-    with inside_bake(bake):
-        # Push to Github
-        subprocess.check_call("git remote add origin git@github.com:dokempf/test-github-actions-cookiecutter-cpp-project.git".split())
-        subprocess.check_call("git push -f origin main".split())
-        bake_sha1 = subprocess.run("git rev-parse HEAD".split(), capture_output=True).stdout.decode().strip()
+    # Make sure that we have pushed a revision to our test repository
+    bake_sha1 = remote_deploy_sha(cookies)
 
-        def rtd_api_request(endpoint):
-            response = requests.get(
-                'https://readthedocs.org/api/v3/projects/test-github-actions-cookiecutter-cpp-project/{}'.format(endpoint),
-                headers={'Authorization': 'token {}'.format(os.getenv('RTD_API_ACCESS_TOKEN'))}
-            )
-            return response.json()
+    def rtd_api_request(endpoint):
+        response = requests.get(
+            'https://readthedocs.org/api/v3/projects/test-github-actions-cookiecutter-cpp-project/{}'.format(endpoint),
+            headers={'Authorization': 'token {}'.format(os.getenv('RTD_API_ACCESS_TOKEN'))}
+        )
+        return response.json()
 
-        # Wait for the webhook to trigger
-        time.sleep(2)
-        last_build_id = rtd_api_request("versions/latest/builds")["results"][0]["id"]
+    # Check that the build has the correct commit
+    last_build_id = rtd_api_request("versions/latest/builds")["results"][0]["id"]
+    build = rtd_api_request('builds/{}'.format(last_build_id))
+    assert build["commit"] == bake_sha1
 
-        # Check that the build has the correct commit
+    # Wait until the build has finished
+    while build['state']['code'] != 'finished':
+        time.sleep(5)
         build = rtd_api_request('builds/{}'.format(last_build_id))
-        assert build["commit"] == bake_sha1
 
-        while build['state']['code'] != 'finished':
-            time.sleep(5)
-            build = rtd_api_request('builds/{}'.format(last_build_id))
-
-        assert build['success']
+    assert build['success']
