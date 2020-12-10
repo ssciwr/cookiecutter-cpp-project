2902ac31d30f083c06b3fd0e53b362e6f3e1fc3d
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2020-12-10 14:39:09 +0100

    Make Pypi testing more robust with respect to previously failed runs

--- tests/test_deploy_bake.py
+++ tests/test_deploy_bake.py
@@ -124,9 +124,14 @@ def test_pypi_deploy(virtualenv):
             return version.parse(response.json()['version'])
         else:
             return version.parse('0.0.0')
+
+    # Construct a version, by finding the maximum version across Github, PyPI and TestPyPI and increasing that
+    gh = github.Github(os.getenv("GH_API_ACCESS_TOKEN"))
+    repo = gh.get_repo('dokempf/test-github-actions-cookiecutter-cpp-project')
     current_version = max(
         upstream_version('https://pypi.org/pypi/testgithubactionscookiecuttercppproject/json'),
-        upstream_version('https://test.pypi.org/pypi/testgithubactionscookiecuttercppproject/json')
+        upstream_version('https://test.pypi.org/pypi/testgithubactionscookiecuttercppproject/json'),
+        version.parse(repo.get_latest_release().title[1:]
     )
     next_version = version.Version('{}.{}.{}'.format(current_version.major, current_version.minor, current_version.micro + 1))
 
@@ -140,9 +145,7 @@ def test_pypi_deploy(virtualenv):
             source.write(re.sub(r'version=.*$', 'version={}'.format(str(next_version)), line))
     subprocess.check_call("git push -f origin main:pypi_release".split())
 
-    # Authenticate with the Github API to create a release
-    gh = github.Github(os.getenv("GH_API_ACCESS_TOKEN"))
-    repo = gh.get_repo('dokempf/test-github-actions-cookiecutter-cpp-project')
+    # Create the release - this will trigger the PyPI release workflow
     repo.create_git_release(
         'v{}'.format(str(next_version)),
         'v{}'.format(str(next_version)),
@@ -151,7 +154,6 @@ def test_pypi_deploy(virtualenv):
     )
 
     # Identify the PyPI release workflow
-    repo = gh.get_repo('dokempf/test-github-actions-cookiecutter-cpp-project')
     branch = repo.get_branch('pypi_release')
     workflow = repo.get_workflow("pypi.yml").get_runs()[0]
     assert workflow.head_sha == branch.commit.sha
